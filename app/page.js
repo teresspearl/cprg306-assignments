53e16b83a975190c651f8685a9efc84695bfa1ce
teresstinga <tpearl2594@gmail.com>
2024-02-06 23:38:12 -0700

    Edit homepage

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Page() {
   return (
     <main>
       <h1 class="m-9 p-5 text-5xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
-      <div><li><Link href="/week-2" class="m-9 ml-11 p-5 text-lg">Week 2 Assignment</Link></li></div>
-      <div><li><Link href="/week-3" class="m-9 ml-11 p-5 text-lg">Week 3 Assignment</Link></li></div>
+      <div><ul><Link href="/week-2" class="m-9 ml-11 p-5 text-lg">Week 2 Assignment</Link></ul></div>
+      <div><ul><Link href="/week-3" class="m-9 ml-11 p-5 text-lg">Week 3 Assignment</Link></ul></div>
     </main>
   );
 }
